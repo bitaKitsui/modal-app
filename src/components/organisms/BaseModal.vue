0fa4408948d15aea511f78d3f782a3ccdfe69a56
bitakitsui <takaki_kitsui@bita.jp>
2022-02-08 08:48:52 +0900

    organisms追加

---
+++ src/components/organisms/BaseModal.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import BaseBody from '../molecules/BaseBody.vue'
+import BaseFooter from '../molecules/BaseFooter.vue'
+</script>
+
+<template>
+  <div>
+    <BaseBody />
+    <BaseFooter />
+  </div>
+</template>
