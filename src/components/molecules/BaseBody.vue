73b29f603045977af61533e06fc0b4bc23bef1ee
bitakitsui <takaki_kitsui@bita.jp>
2022-02-08 08:47:03 +0900

    feat: molecules追加

---
+++ src/components/molecules/BaseBody.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import BaseHeader from '../atoms/BaseHeader.vue'
+import BaseButton from '../atoms/BaseButton.vue'
+import BaseContent from '../atoms/BaseContent.vue'
+</script>
+
+<template>
+  <div>
+    <BaseHeader :value="'Modal Title'" />
+    <BaseButton
+      :label="'×'"
+      :type="'button'"
+      :color="'secondary'"
+      :outlined="false"
+    />
+    <BaseContent
+      :value="'Sit nulla est ex deserunt exercitation anim occaecat.'"
+    />
+  </div>
+</template>
---
+++ src/components/molecules/BaseFooter.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import BaseButton from '../atoms/BaseButton.vue'
+</script>
+
+<template>
+  <div>
+    <BaseButton
+      :label="'Close'"
+      :type="'button'"
+      :color="'primary'"
+      :outlined="true"
+    />
+    <BaseButton
+      :label="'Secondary Action'"
+      :type="'button'"
+      :color="'secondary'"
+      :outlined="false"
+    />
+  </div>
+</template>
