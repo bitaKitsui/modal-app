4459d086bac2fcb22846e0d387f05dc53d3dc9ef
bitakitsui <takaki_kitsui@bita.jp>
2022-02-08 08:51:39 +0900

    fix: App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BaseButton from './components/atoms/BaseButton.vue'
+import BaseModal from './components/organisms/BaseModal.vue'
+</script>
 
 <template>
-  <div>Modal</div>
+  <div>
+    <BaseButton
+      :label="'Open Modal'"
+      :type="'button'"
+      :color="'secondary'"
+      :outlined="true"
+    />
+    <BaseModal />
+  </div>
 </template>
 
 <style></style>
