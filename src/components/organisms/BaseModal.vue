575d1a1b3dcb6798a1d2e032b50608fc6f429ce0
bita-kitsui <takaki_kitsui@bita.jp>
2022-02-10 07:58:09 +0900

    feat: modalをbooleanで切り替え

--- src/components/organisms/BaseModal.vue
+++ src/components/organisms/BaseModal.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import BaseBody from '../molecules/BaseBody.vue'
 import BaseFooter from '../molecules/BaseFooter.vue'
 import BaseHeader from '../molecules/BaseHeader.vue'
 import { MODAL_BODY } from '../../constants'
+
+const isActive = ref(false)
 </script>
 
 <template>
-  <div class="modal">
+  <div v-if="isActive" class="modal">
     <BaseHeader :header-title="'Modal Title'" />
     <BaseBody :body="MODAL_BODY" />
     <BaseFooter />
